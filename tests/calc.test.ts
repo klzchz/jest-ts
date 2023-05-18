43c822f0ffa1af6f2033767dcddb6c680319f9ec
Lucas Andrade <lucascorporation@live.com>
2023-05-17 23:03:36 -0300

    playing with tests

--- tests/calc.test.ts
+++ tests/calc.test.ts
@@ -1,5 +1,5 @@
 import { describe } from 'node:test';
-import {add} from '../src/calc';
+import {add,multiply} from '../src/calc';
 
 
 describe("Test CALC",()=>{
@@ -10,6 +10,20 @@ describe("Test CALC",()=>{
   it("Should return 5 for add(2,3)",()=>{
     expect(add(2,3)).toBe(5);
 });
+});
+
+
+describe("Test CALC Multiply",()=>{
+  it("Should return 15 for multiply(10,5)",()=>{
+      expect(multiply(4,5)).toBe(20);
+  });
+
+  it("Should return 5 for multiply(2,3)",()=>{
+    expect(multiply(5,10)).toBe(50);
+});
 })
+
+
+
 
 
